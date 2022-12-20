e8c37a4535fab90a959c2b54feccf69c7592f4b4
BuildTools <unconfigured@null.spigotmc.org>
2022-12-19 21:32:17 -0300

    Day 3: Time Conversion

--- Week1/Day3.ts
+++ Week1/Day3.ts
@@ -1,25 +1,26 @@
 'use strict';
 
-const fs = require('fs');
-
+import { WriteStream, createWriteStream } from "fs";
 process.stdin.resume();
 process.stdin.setEncoding('utf-8');
 
-let inputString = '';
-let currentLine = 0;
+let inputString: string = '';
+let inputLines: string[] = [];
+let currentLine: number = 0;
 
-process.stdin.on('data', function(inputStdin) {
+process.stdin.on('data', function(inputStdin: string): void {
     inputString += inputStdin;
 });
 
-process.stdin.on('end', function() {
-    inputString = inputString.split('\n');
+process.stdin.on('end', function(): void {
+    inputLines = inputString.split('\n');
+    inputString = '';
 
     main();
 });
 
-function readLine() {
-    return inputString[currentLine++];
+function readLine(): string {
+    return inputLines[currentLine++];
 }
 
 /*
@@ -29,17 +30,27 @@ function readLine() {
  * The function accepts STRING s as parameter.
  */
 
-function timeConversion(s) {
-    // Write your code here
-
+function timeConversion(s: string): string {
+    let hourFormat: string = s.slice(8), hour: number = parseInt(s.slice(0, 2));
+    let convertedString: string;
+    
+    if(hourFormat == "PM" && hour < 12) {
+        convertedString = (hour + 12) + s.slice(2, -2);
+    } else if(hourFormat == "AM" && hour == 12) {
+        convertedString = "00" + s.slice(2, -2);
+    } else {
+        convertedString = s.slice(0, -2);
+    }
+    
+    return(convertedString);
 }
 
 function main() {
-    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+    const ws: WriteStream = createWriteStream(process.env['OUTPUT_PATH']);
 
-    const s = readLine();
+    const s: string = readLine();
 
-    const result = timeConversion(s);
+    const result: string = timeConversion(s);
 
     ws.write(result + '\n');
 
