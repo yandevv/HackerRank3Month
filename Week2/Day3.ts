117e8402dfc30439d0507d2a037d6fd02c8b2138
Yan Gomes <ybritogomes@gmail.com>
2023-03-09 18:38:03 -0300

    Add: Added Exercise of Day 3 / Week 2 Resolved

---
+++ Week2/Day3.ts
@@ -0,0 +1,55 @@
+'use strict';
+
+import { WriteStream, createWriteStream } from "fs";
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString: string = '';
+let inputLines: string[] = [];
+let currentLine: number = 0;
+
+process.stdin.on('data', function(inputStdin: string): void {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function(): void {
+    inputLines = inputString.split('\n');
+    inputString = '';
+
+    main();
+});
+
+function readLine(): string {
+    return inputLines[currentLine++];
+}
+
+/*
+ * Complete the 'flippingBits' function below.
+ *
+ * The function is expected to return a LONG_INTEGER.
+ * The function accepts LONG_INTEGER n as parameter.
+ */
+
+function flippingBits(n: number): number {
+    let flippedInteger: number;
+    
+    flippedInteger = 4294967295 - n;
+    
+    return flippedInteger;
+}
+
+function main() {
+    const ws: WriteStream = createWriteStream(process.env['OUTPUT_PATH']);
+
+    const q: number = parseInt(readLine().trim(), 10);
+
+    for (let qItr: number = 0; qItr < q; qItr++) {
+        const n: number = parseInt(readLine().trim(), 10);
+
+        const result: number = flippingBits(n);
+
+        ws.write(result + '\n');
+    }
+
+    ws.end();
+}
